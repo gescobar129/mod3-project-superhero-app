59354cb5f4b64285e85dccaf995d73bc2a4e2f02
Gaida Escobar <gescobar129@gmail.com>
2019-11-11 16:11:47 -0500

    added create feature

--- backend/app/controllers/superheros_controller.rb
+++ backend/app/controllers/superheros_controller.rb
@@ -8,4 +8,20 @@ class SuperherosController < ApplicationController
         @superhero = Superhero.find(params[:id])
         render json: @superhero
     end
+
+    def create 
+        @superhero = Superhero.create(superhero_params)
+        if @superhero.valid? 
+            render json: @superhero, status: 201
+        else
+            render json: {errors: @toy.errors.full_messages}, status: 401
+        end
+    end
+
+private 
+
+def superhero_params
+    params.permit(:name, :intelligence, :strength, :speed, :durability, :power, :combat, :full_name, :place_of_birth, :height_feet, :weight_lbs, :eye_color, :hair_color, :occupation, :relatives, :image_url)
+end
+
 end
--- frontend/index.js
+++ frontend/index.js
@@ -43,7 +43,6 @@ function renderCharacterCards(character) {
     button.append(characterCard) //I wrapped the characterCard in a button so modal can work
     characterContainer.append(button)
 
-
     button.addEventListener('click', event => {
         //following is what happens when you click a character card
         
@@ -61,4 +60,61 @@ function renderCharacterCards(character) {
 
         // debugger
     })
+
+    
 }
+
+let createButtonDiv = document.querySelector('.create-button-div')
+let createButton = document.createElement('button')
+createButton.setAttribute('type', 'button')
+createButton.setAttribute('data-toggle', 'modal')
+createButton.setAttribute('data-target', '#exampleModal')
+
+createButton.innerText = "Create Character"
+createButtonDiv.append(createButton)
+
+createButton.addEventListener('click', event => {
+
+    let saveForm = document.querySelector('.create-form') 
+
+    saveForm.addEventListener('submit', event => {
+        event.preventDefault();
+        event.target['create-character-name'].value
+        // debugger
+    
+    fetch('http://localhost:3000/superheros', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify({
+            name: event.target['create-character-name'].value,
+            image_url: event.target["create-image-url"].value,
+            full_name: event.target["create-character-full-name"].value,
+            // name: event.target["bio-text"].value,
+            inetelligence: event.target["create-character-intelligence"].value,
+            strength: event.target["create-character-strength"].value,
+            speed: event.target["create-character-speed"].value,
+            durability: event.target['create-character-durability'].value,
+            power: event.target['create-character-power'].value,
+            combat: event.target['create-character-combat'].value,
+            height_feet: event.target['create-character-height'].value,
+            weight_lbs: event.target['create-character-weight'].value,
+            eye_color: event.target['create-character-eye-color'].value,
+            hair_color: event.target['create-character-hair-color'].value,
+            universe: event.target['create-character-universe'].value,
+            occupation: event.target['create-character-occupation'].value,
+            place_of_birth: event.target['create-character-birth'].value,
+            relatives: event.target['create-character-relatives'].value,
+        })
+    })
+    .then(res => res.json())
+    .then(newCharObj => {
+        // debugger
+        renderCharacterCards(newCharObj)
+    })
+
+})
+
+})
